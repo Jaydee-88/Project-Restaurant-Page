7f316d8592b60110869356d7a34a724de754fcf7
jaydee-88 <jevworkspace@gmail.com>
2024-12-16 23:55:08 +0400

    implementing UI and JS

--- src/homepage.js
+++ src/homepage.js
@@ -1,4 +1,17 @@
 export function loadHomePage() {
   const contentDiv = document.querySelector("#content");
-  console.log("hello Content");
+
+  const heroTextDiv = document.createElement("div");
+  const headerPara1 = document.createElement("h1");
+  const headerPara2 = document.createElement("h1");
+  const heroButton = document.createElement("button");
+  headerPara1.textContent = "Think, Plan, and Track";
+  headerPara2.textContent = "all in one place";
+  heroTextDiv.appendChild(headerPara1);
+  heroTextDiv.appendChild(headerPara2);
+  heroTextDiv.classList.add("heroText");
+  heroButton.textContent = "Get Free Demo";
+
+  contentDiv.appendChild(heroTextDiv);
+  contentDiv.appendChild(heroButton);
 }
