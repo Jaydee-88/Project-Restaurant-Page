f96b79228169f35a01b053e912bcbd7ee20e157b
jaydee-88 <jevworkspace@gmail.com>
2024-12-19 00:09:09 +0400

    Function testing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import "./styles.css";
 import { loadHomePage } from "./homepage.js";
 
+const contentDiv = document.querySelector("#content");
 const homeButton = document.querySelector("#home");
 const menuButton = document.querySelector("#menu");
 const aboutButton = document.querySelector("#about");
 const contactUsButton = document.querySelector("#contactUs");
+const navButtons = document.querySelectorAll("nav button");
 
-console.log("hello");
-window.onload = loadHomePage;
+contentDiv.innerHTML = "";
+
+// make a class that defaults load the home button. then the rest out blank and load their respective JS scripts.
+
+menuButton.addEventListener("click", (e) => {
+  console.log("clicked");
+  navButtons.forEach((el) => {
+    el.classList.remove("buttonActive");
+  });
+  menuButton.className = "buttonActive";
+});
+loadHomePage();
