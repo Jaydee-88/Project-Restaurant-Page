87068bffd5380dc1b3b6fbf69c4e7ec9c554bff3
jaydee-88 <jevworkspace@gmail.com>
2025-01-16 21:03:22 +0400

    Need to Fix Timing UI

--- src/menupage.js
+++ src/menupage.js
@@ -6,9 +6,31 @@ export function loadMenuPage() {
 
   contentDiv.innerHTML = "";
 
+  const timingDiv = document.createElement("div");
+  timingDiv.style.display = "flex";
+
+  const scheduleArray = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  // display text in scheduleArray and append in timingDiv
+  for (let i = 0; i < scheduleArray.length; i++) {
+    // create text element and append each days
+    const header2 = document.createElement("h2");
+    header2.textContent = scheduleArray[i];
+    timingDiv.appendChild(header2);
+  }
+
   headerPara1.textContent = "Put Menu here";
   headerPara2.textContent = "List all menu and use ul & li";
 
+  menuTextDiv.appendChild(timingDiv);
   menuTextDiv.appendChild(headerPara1);
   menuTextDiv.appendChild(headerPara2);
   contentDiv.appendChild(menuTextDiv);
